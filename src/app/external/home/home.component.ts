a498d08d0382f4fa390ad485e1818834acf2d911
vitormoschetta <vitormoschetta@gmail.com>
2020-12-28 11:25:13 -0400

    navbar update style

--- src/app/external/home/home.component.ts
+++ src/app/external/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {
+    if (this.authService.currentUserValue) {  // <-- redirecionar para dashboard se já estiver logado
+      this.router.navigate(['/dashboard']);
+    }
+   }
 
   ngOnInit(): void {
   }
