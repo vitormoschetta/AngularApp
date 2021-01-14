d33db46c41c6d78571e9e4940b77539266a7a52e
vitormoschetta <vitormoschetta@gmail.com>
2021-01-13 23:17:16 -0400

    add guarda d rotas em dashboard

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
 
-const routes: Routes = [ 
+const routes: Routes = [   
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]  },  
   { path: 'unauthorized', component: UnauthorizedComponent },  
-  { path: 'dashboard', component: DashboardComponent },  
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
